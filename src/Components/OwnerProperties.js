82c6e5da591c2ad96d9a03260427a213a61fa518
amany-abdelrahman <amanyabdelrahmansalah0@gmail.com>
2024-02-24 01:10:50 +0200

    Update OwnerProperties.js file

--- src/Components/OwnerProperties.js
+++ src/Components/OwnerProperties.js
@@ -7,59 +7,18 @@ const OwnerProperties = () => {
     const [properties, setProperties] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const propertiesPerPage = 8;
-    const [selectedCategory, setSelectedCategory] = useState(null);
-
-    const groupPropertiesByCategory = (allProperties) => {
-        const groupedProperties = {};
-        allProperties.forEach(property => {
-            const category = property.category;
-            if (!groupedProperties[category]) {
-                groupedProperties[category] = [];
-            }
-            groupedProperties[category].push(property);
-        });
-        return groupedProperties;
-    };
-
-    const shuffleArray = (array) => {
-        const shuffledArray = [...array];
-        for (let i = shuffledArray.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-        }
-        return shuffledArray;
-    };
-
-    const shufflePropertiesWithinCategories = (groupedProperties) => {
-        const shuffledProperties = {};
-        for (const category in groupedProperties) {
-            shuffledProperties[category] = shuffleArray(groupedProperties[category]);
-        }
-        return Object.values(shuffledProperties).flat();
-    };
-
     useEffect(() => {
         axios.get('https://dummyjson.com/products')
             .then(response => {
                 const data = response.data;
-                const groupedProperties = groupPropertiesByCategory(data.products);
-                const categories = Object.keys(groupedProperties);
-                const randomCategory = categories[Math.floor(Math.random() * categories.length)];
-                const shuffledProperties = shufflePropertiesWithinCategories(groupedProperties);
-                setProperties(shuffledProperties);
-                setSelectedCategory(randomCategory);
+                setProperties(data.products); 
             })
             .catch(error => console.error(error));
     }, []);
-
     const indexOfLastProperty = currentPage * propertiesPerPage;
     const indexOfFirstProperty = indexOfLastProperty - propertiesPerPage;
-    const currentProperties = selectedCategory
-        ? properties.filter(property => property.category === selectedCategory).slice(indexOfFirstProperty, indexOfLastProperty)
-        : [];
-
+    const currentProperties = properties.slice(indexOfFirstProperty, indexOfLastProperty);
     const paginate = pageNumber => setCurrentPage(pageNumber);
-
     return (
         <div>
             <Container className="d-flex flex-column align-items-center mt-5" style={{ height: '100vh' }}>
@@ -90,17 +49,15 @@ const OwnerProperties = () => {
                         ))}
                     </tbody>
                 </Table>
-                {currentProperties.length > propertiesPerPage && (
-                    <div className="d-flex justify-content-end">
-                        <Pagination>
-                            {Array.from({ length: Math.ceil(currentProperties.length / propertiesPerPage) }).map((_, index) => (
-                                <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => paginate(index + 1)}>
-                                    {index + 1}
-                                </Pagination.Item>
-                            ))}
-                        </Pagination>
-                    </div>
-                )}
+                <div className="d-flex justify-content-end">
+                    <Pagination>
+                        {Array.from({ length: Math.ceil(properties.length / propertiesPerPage) }).map((_, index) => (
+                            <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => paginate(index + 1)}>
+                                {index + 1}
+                            </Pagination.Item>
+                        ))}
+                    </Pagination>
+                </div>
             </Container>
         </div>
     );
