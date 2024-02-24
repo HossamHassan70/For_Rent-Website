a7d4a90bf9aa548e67e617f2e058d6ede526e90c
AmrAboelmagdhussien <Amraboelmagdhussien@gmail.com>
2024-02-24 17:25:59 +0200

    DD

--- src/MyStore/store.js
+++ src/MyStore/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/combinedReducers";
-import thunk  from "redux-thunk";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(
