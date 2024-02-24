af0c3f2d7a8513a53c0fff354efeb6e5dbb0b981
Mikerosoft99 <kerosamy8000@gmail.com>
2024-02-24 19:25:11 +0200

    Fixed a bug in store.js

--- src/MyStore/store.js
+++ src/MyStore/store.js
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/combinedReducers';
 import thunk from 'redux-thunk';
-import {thunk}  from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(
