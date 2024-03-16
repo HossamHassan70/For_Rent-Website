776c3ff9d8816fb14ed94e645175ad70a2d4991a
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-17 00:31:15 +0200

    Deleted repetitive file & Fixed repetitive code in ViewProperty.css

--- src/pages/ViewProperty.jsx
+++ src/pages/ViewProperty.jsx
@@ -12,7 +12,7 @@ import {
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./ViewProperty.css";
+import "./css/ViewProperty.css";
 import ReviewsList from "../Components/reviews";
 
 const PropertyView = () => {
