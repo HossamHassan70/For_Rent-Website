4c6f94d8d9c1bc4803a7cbcbf1e863497d4bbe2e
AmrAboelmagdhussien <Amraboelmagdhussien@gmail.com>
2024-03-19 15:49:29 +0200

    Fixed Auth Regarding logged in

--- src/App.js
+++ src/App.js
@@ -1,67 +1,6 @@
-// import NavigationBar from "./Components/Navbar";
-// import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-// import SignUp from "./pages/register";
-// import LoginPre from "./pages/login";
-// import Footer from "./Components/Footer";
-// import HomePage from "./pages/homePage";
-// import PropertyView from "./pages/ViewProperty";
-// import UserProfile from "./pages/userProfile";
-// import OwnerProperties from "./Components/OwnerProperties";
-// import PageNotFound from "./pages/PageNotFound";
-// import { useSelector } from "react-redux";
-// import store, { persistor } from "./MyStore/store";
-// import { PersistGate } from 'redux-persist/integration/react';
-
-// // const isAuthenticated = () => {
-// //   return sessionStorage.getItem("refreshToken") !== null;
-// // };
-// // const PrivateRoute = ({ element: Element, ...rest }) => {
-// //   return isAuthenticated() ? Element : <Navigate to="/login" replace />;
-// // };
-// function App() {
-//   const PrivateRoute = ({ element: Component, ...rest }) => {
-//     const isLoggedIn = useSelector((state) => state.authReducer.isLoggedIn);
-
-//     return isLoggedIn ? (
-//       <Component {...rest} />
-//     ) : (
-//       <Navigate to="/login" replace />
-//     );
-//   };
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <NavigationBar />
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/login" element={<LoginPre />} />
-//           <Route path="/register" element={<SignUp />} />
-//           <Route
-//             path="/property/:id"
-//             element={<PrivateRoute element={PropertyView} />}
-//           />
-//           <Route
-//             path="/user/:userId"
-//             element={<PrivateRoute element={UserProfile} />}
-//           />
-//           <Route
-//             path="/OwnerProperties"
-//             element={<PrivateRoute element={OwnerProperties} />}
-//           />
-//           {/* <Route path="/wishlist" element={<Fave />} /> */}
-//           <Route path="*" element={<PageNotFound />} />
-//         </Routes>
-//         <Footer />
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-// export default App;
-
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./MyStore/store";
 import NavigationBar from "./Components/Navbar";
@@ -74,8 +13,13 @@ import UserProfile from "./pages/userProfile";
 import OwnerProperties from "./Components/OwnerProperties";
 import PageNotFound from "./pages/PageNotFound";
 
-const App = () => {
 
+const AuthRoute = ({ children }) => {
+  const isLoggedIn = useSelector((state) => state.authReducer.isLoggedIn);
+  return isLoggedIn ? <Navigate to="/" /> : children;
+};
+
+const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -83,30 +27,44 @@ const App = () => {
           <NavigationBar />
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPre />} />
-            <Route path="/register" element={<SignUp />} />
+            <Route
+              path="/login"
+              element={
+                <AuthRoute>
+                  <LoginPre />
+                </AuthRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <AuthRoute>
+                  <SignUp />
+                </AuthRoute>
+              }
+            />
             <Route
               path="/property/:id"
               element={
-                <ProtectedRoute>
+                <PrivateRoute>
                   <PropertyView />
-                </ProtectedRoute>
+                </PrivateRoute>
               }
             />
             <Route
               path="/user/:userId"
               element={
-                <ProtectedRoute>
+                <PrivateRoute>
                   <UserProfile />
-                </ProtectedRoute>
+                </PrivateRoute>
               }
             />
             <Route
               path="/OwnerProperties"
               element={
-                <ProtectedRoute>
+                <PrivateRoute>
                   <OwnerProperties />
-                </ProtectedRoute>
+                </PrivateRoute>
               }
             />
             <Route path="*" element={<PageNotFound />} />
@@ -118,7 +76,7 @@ const App = () => {
   );
 };
 
-function ProtectedRoute({ children }) {
+function PrivateRoute({ children }) {
   const isLoggedIn = useSelector((state) => state.authReducer.isLoggedIn);
   return isLoggedIn ? children : <Navigate to="/login" replace />;
 }
