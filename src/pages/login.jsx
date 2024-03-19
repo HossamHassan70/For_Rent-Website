0d39a76229a9241c0ef0b1866dc9d3904d8b1801
AmrAboelmagdhussien <Amraboelmagdhussien@gmail.com>
2024-03-19 14:26:43 +0200

    Fixed Ui Bugs

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -193,22 +193,92 @@ export default function LoginPre() {
   });
 
   return (
+    // <div className="d-flex justify-content-between align-items-center flex-wrap">
+    //   <Col xs={12} lg={6}>
+    //     <Image
+    //       src={loginSignUp}
+    //       className="img-fluid"
+    //       alt="Login Illustration"
+    //     />
+    //   </Col>
+    //   <Col xs={12} lg={6}>
+    //     <div className="login-form-container">
+    //       <h2>Login to ForRent</h2>
+    //       <p>Enter Your Details Below</p>
+    //       {formik.errors.server && (
+    //         <AlertCom errorType="danger" errorMsg={formik.errors.server} />
+    //       )}
+    //       <Form noValidate onSubmit={formik.handleSubmit}>
+    //         <Form.Group className="mb-3">
+    //           <Form.Control
+    //             id="uName"
+    //             name="uName"
+    //             type="text"
+    //             placeholder="Username"
+    //             value={formik.values.uName}
+    //             onChange={formik.handleChange}
+    //             onBlur={formik.handleBlur}
+    //             isInvalid={!!formik.errors.uName && formik.touched.uName}
+    //           />
+    //           <Form.Control.Feedback type="invalid">
+    //             {formik.errors.uName}
+    //           </Form.Control.Feedback>
+    //         </Form.Group>
+
+    //         <Form.Group className="mb-3">
+    //           <Form.Control
+    //             id="password"
+    //             name="password"
+    //             type="password"
+    //             placeholder="Password"
+    //             value={formik.values.password}
+    //             onChange={formik.handleChange}
+    //             onBlur={formik.handleBlur}
+    //             isInvalid={!!formik.errors.password && formik.touched.password}
+    //           />
+    //           <Form.Control.Feedback type="invalid">
+    //             {formik.errors.password}
+    //           </Form.Control.Feedback>
+    //         </Form.Group>
+
+    //         <div className="d-flex justify-content-between">
+    //           <BtnsCo btnType="submit" btnCo="primary" btnText="Login" />
+    //           <Link to="/forgot-password">Forget Your password?</Link>
+    //         </div>
+    //       </Form>
+    //       <p className="pt-3">
+    //         Didn't Register Yet? <Link to="/register">Signup</Link>
+    //       </p>
+    //     </div>
+    //   </Col>
+    // </div>
     <div className="d-flex justify-content-between align-items-center flex-wrap">
       <Col xs={12} lg={6}>
         <Image
           src={loginSignUp}
-          className="img-fluid"
+          className="m-0 img-fluid position-relative w-100 h-auto h-lg-100"
           alt="Login Illustration"
+          style={{ maxWidth: "auto", maxHeight: "900px" }}
         />
+        <div className="cs-posti position-absolute">
+          <h3 className="text-white">For Rent</h3>
+          <p className="text-white">
+            Welcome to ForRent Website Where Renting buying and Selling is
+            Easier
+          </p>
+        </div>
       </Col>
       <Col xs={12} lg={6}>
-        <div className="login-form-container">
-          <h2>Login to ForRent</h2>
-          <p>Enter Your Details Below</p>
-          {formik.errors.server && (
-            <AlertCom errorType="danger" errorMsg={formik.errors.server} />
-          )}
+        <div className="container d-flex flex-column gap-3 pt-3">
+          <div className="d-flex flex-column">
+            <h2 className="m-0 align-self-start">Login to ForRent</h2>
+            <p className="m-0 align-self-start">Enter Your Details Below</p>
+            {formik.errors.server && (
+              <AlertCom errorType="danger" errorMsg={formik.errors.server} />
+            )}
+          </div>
           <Form noValidate onSubmit={formik.handleSubmit}>
+            {/* Form groups and controls remain the same */}
             <Form.Group className="mb-3">
               <Form.Control
                 id="uName"
@@ -225,6 +295,7 @@ export default function LoginPre() {
               </Form.Control.Feedback>
             </Form.Group>
 
+            
             <Form.Group className="mb-3">
               <Form.Control
                 id="password"
@@ -240,15 +311,25 @@ export default function LoginPre() {
                 {formik.errors.password}
               </Form.Control.Feedback>
             </Form.Group>
-
-            <div className="d-flex justify-content-between">
-              <BtnsCo btnType="submit" btnCo="primary" btnText="Login" />
-              <Link to="/forgot-password">Forget Your password?</Link>
+            <div className="container d-flex justify-content-center align-content-center gap-3">
+              <BtnsCo
+                btnType="submit"
+                btnCo="primary"
+                btnCs={{ backgroundColor: "#008f97" }}
+                btnText="Login"
+              />
+              <Link
+                to="/forgot-password"
+                className="btn btn-primary"
+                style={{ backgroundColor: "#008f97" }}
+              >
+                Forget Your password?
+              </Link>
             </div>
+            <p className="pt-3">
+              Didn't Register Yet? <Link to="/register">Signup</Link>
+            </p>
           </Form>
-          <p className="pt-3">
-            Didn't Register Yet? <Link to="/register">Signup</Link>
-          </p>
         </div>
       </Col>
     </div>
