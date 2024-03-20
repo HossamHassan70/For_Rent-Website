8cbb926719859456cfffb148f3cfe85e7e628552
safaessam <safaessam610@gmail.com>
2024-03-20 03:46:52 +0200

    add date_published to property list

--- src/pages/propertiesList.jsx
+++ src/pages/propertiesList.jsx
@@ -6,37 +6,38 @@ import LoadingScreen from './loadingScreen';
 import './css/propertiesList.css';
 import Pagination from 'react-js-pagination';
 import { Link } from 'react-router-dom';
+import { formatDistanceToNow, parseISO } from 'date-fns';
 
-
+const formatDate = (dateString) => {
+  const date = parseISO(dateString);
+  return formatDistanceToNow(date, { addSuffix: true });
+};
 const PropertiesList = () => {
   const dispatch = useDispatch();
   const error = useSelector((state) => state.properties.error);
   const loading = useSelector((state) => state.properties.loading);
   const properties = useSelector((state) => state.properties.properties);
-
+  
   const [currentPage, setCurrentPage] = useState(1);
   const [propertiesPerPage] = useState(10);
-
+  
   useEffect(() => {
     dispatch(fetchProperties());
   }, [dispatch]);
-
+  
   if (loading) {
     return (
       <LoadingScreen />
-      
-
-    );
-  }
-
-  if (error) {
-    return <div className='error-page text-danger'>{error}</div>;
-  }
-
-  if (!properties || properties.length === 0) {
+      );
+    }
+    
+    if (error) {
+      return <div className='error-page text-danger'>{error}</div>;
+    }
+    
+    if (!properties || properties.length === 0) {
     return <div>No properties found.</div>;
   } 
- 
 
   const indexOfLastProperty = currentPage * propertiesPerPage;
   const indexOfFirstProperty = indexOfLastProperty - propertiesPerPage;
@@ -58,9 +59,7 @@ const PropertiesList = () => {
                   <Col xs={12} md={6}>
                     <div className="card-img-container position-relative">
                       <img className='prop-image' src={product.image} alt={product.title} />
-                      <div className="favorites-icon position-absolute">
-                        <i className="far fa-heart text-light"></i>
-                      </div>
+                     
                     </div>
                   </Col>
                   <Col xs={12} md={6}>
@@ -72,7 +71,7 @@ const PropertiesList = () => {
                       <span>Price: ${product.price}</span>
                       <span>7.25% CAP</span>
                     </Card.Text>
-                    <Card.Text className='mx-2 since'>4 days ago</Card.Text>
+                    <Card.Text > Date_Published:{formatDate(product.created_at)}</Card.Text>
                     <Link to={`/property/${product.id}`}>
                       <button className='btn more-details'><b>More Details</b></button>
                     </Link>
