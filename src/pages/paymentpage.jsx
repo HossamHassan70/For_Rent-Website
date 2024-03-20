6fc91714529861659963f50cf54bf7f58fafe7b7
AmrAboelmagdhussien <Amraboelmagdhussien@gmail.com>
2024-03-20 02:34:46 +0200

    Paypal Api Added

--- src/pages/paymentpage.jsx
+++ src/pages/paymentpage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useFormik } from "formik";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ValidSchema from "../schemas/paymentpage";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
 
 const PaymentPage = () => {
   const getCardType = (number) => {
