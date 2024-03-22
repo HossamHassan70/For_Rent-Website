a3249fde0a026f2debb85af320c60a95bd32bb1a
AmrAboelmagdhussien <Amraboelmagdhussien@gmail.com>
2024-03-22 18:46:22 +0200

    Fixed Payment

--- src/pages/paymentpage.jsx
+++ src/pages/paymentpage.jsx
@@ -56,7 +56,7 @@ const PaymentPage = () => {
         <div className="vh-100 d-flex justify-content-center align-items-center flex-column">
           {" "}
           <h1>You Don't Have any accepted requests</h1>
-          <i class="fa-solid fa-ticket"></i>
+          <i class="iconz fa-solid fa-ticket"></i>
         </div>
       </>
     );
